ad589eae20a1d4a21d2f3a859902cf7fd86f6ac6
Erick Christian <erickchristian48@gmail.com>
2025-02-06 20:35:44 +0700

    feat: Add timeout option to page.goto in scrapeHtml function

--- src/lib/scrape.ts
+++ src/lib/scrape.ts
@@ -3,7 +3,9 @@ import { createPage } from "./browser"
 export async function scrapeHtml(url: string): Promise<string> {
   const page = await createPage()
   try {
-    await page.goto(url)
+    await page.goto(url, {
+      timeout: 60_000,
+    })
 
     const html = await page.content()
 
